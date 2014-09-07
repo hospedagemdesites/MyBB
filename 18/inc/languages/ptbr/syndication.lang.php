cc9ebebccd15f0f507ff3f3fc4e406fb17d7903c
Leandro dos Santos <leandrors.sp@gmail.com>
2014-09-07 19:33:20 -0300

    Novo arquivo traduzido...

--- 18/inc/languages/ptbr/syndication.lang.php
+++ 18/inc/languages/ptbr/syndication.lang.php
@@ -2,12 +2,12 @@
 /**
  * MyBB 1.8 Pacote de Idioma Português do Brasil
  * Copyright 2014 MyBB Group, All Rights Reserved
- * Traduzido por "" ""
+ * Traduzido por "Leandro dos Santos" "www.hospedagemecriacaodesites.com.br"
  */
 
-$l['all_forums'] = "All Forums";
-$l['forum'] = "Forum:";
-$l['posted_by'] = "Posted By:";
-$l['on'] = "on";
+$l['all_forums'] = "Todos Fóruns";
+$l['forum'] = "Fórum:";
+$l['posted_by'] = "Postado por:";
+$l['on'] = "em";
 $l['portal'] = "Portal";
 
