aba45516d79f975bebf38be7258e9da8bb47bd5e
Leandro dos Santos <leandrors.sp@gmail.com>
2014-09-07 18:46:24 -0300

    Novo arquivo traduzido...

--- 18/inc/languages/ptbr/sendthread.lang.php
+++ 18/inc/languages/ptbr/sendthread.lang.php
@@ -2,19 +2,19 @@
 /**
  * MyBB 1.8 Pacote de Idioma Português do Brasil
  * Copyright 2014 MyBB Group, All Rights Reserved
- * Traduzido por "" ""
+ * Traduzido por "Leandro dos Santos" "www.hospedagemecriacaodesites.com.br"
  */
 
-$l['nav_sendthread'] = "Send Thread to a Friend";
+$l['nav_sendthread'] = "Enviar Tópico a um Amigo";
 
-$l['send_thread'] = "Send To Friend";
-$l['recipient'] = "Recipient:";
-$l['recipient_note'] = "Enter your friends email address here.";
-$l['subject'] = "Subject:";
-$l['message'] = "Message:";
-$l['image_verification'] = "Image Verification";
-$l['verification_subnote'] = "(case insensitive)";
-$l['verification_note'] = "Please enter the text contained within the image into the text box below it. This process is used to prevent automated processes.";
-$l['error_nosubject'] = "You are required to enter a subject for the message to send your thread";
-$l['error_nomessage'] = "You are required to enter a message before you can send this thread to your friend";
+$l['send_thread'] = "Enviar a um Amigo";
+$l['recipient'] = "Destinatário:";
+$l['recipient_note'] = "Insira os Endereços de E-mail dos seus amigos aqui.";
+$l['subject'] = "Assunto:";
+$l['message'] = "Mensagem:";
+$l['image_verification'] = "Imagem de Verificação";
+$l['verification_subnote'] = "(maiúsculas e minúsculas)";
+$l['verification_note'] = "Por favor, insira o texto contido dentro da imagem na caixa de texto abaixo. Este processo é usado para evitar processos automatizados.";
+$l['error_nosubject'] = "Você deve informar um assunto para a mensagem para enviar seu tópico";
+$l['error_nomessage'] = "É necessário inserir uma mensagem antes de enviar este tópico ao seu amigo";
 
