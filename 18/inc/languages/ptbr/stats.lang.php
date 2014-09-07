1a320836c526ca81d00d2effd38c12d7bd7c4bce
Leandro dos Santos <leandrors.sp@gmail.com>
2014-09-07 19:32:17 -0300

    Novo arquivo traduzido...

--- 18/inc/languages/ptbr/stats.lang.php
+++ 18/inc/languages/ptbr/stats.lang.php
@@ -2,36 +2,36 @@
 /**
  * MyBB 1.8 Pacote de Idioma Português do Brasil
  * Copyright 2014 MyBB Group, All Rights Reserved
- * Traduzido por "" ""
+ * Traduzido por "Leandro dos Santos" "www.hospedagemecriacaodesites.com.br"
  */
 
-$l['nav_stats'] = "Board Statistics";
-$l['stats_disabled'] = "You cannot view the statistics page as it has been disabled by the Administrator.";
+$l['nav_stats'] = "Estatísticas do Fórum";
+$l['stats_disabled'] = "Você não pode visualizar a página de estatísticas, pois ela foi desativada pelo administrador.";
 
-$l['board_stats'] = "Board Statistics";
-$l['none'] = "None";
-$l['totals'] = "Totals";
-$l['averages'] = "Averages";
-$l['posts'] = "Posts:";
-$l['threads'] = "Threads:";
-$l['members'] = "Members:";
-$l['ppd'] = "Posts per day:";
-$l['tpd'] = "Threads per day:";
-$l['mpd'] = "Members per day:";
-$l['ppm'] = "Posts per member:";
-$l['tpm'] = "Threads per member:";
-$l['rpt'] = "Replies per thread:";
-$l['no_posts'] = "No";
-$l['general'] = "General";
-$l['newest_member'] = "Newest Member:";
-$l['members_posted'] = "Members who have posted:";
-$l['todays_top_poster'] = "Today's top poster: <b>{1}</b> (<b>{2}</b> posts)";
-$l['popular_forum'] = "Most popular forum: <b>{1}</b> (<b>{2}</b> posts, <b>{3}</b> threads)";
-$l['most_popular'] = "Most Popular...";
-$l['most_replied_threads'] = "Most Replied To Threads";
-$l['most_viewed_threads'] = "Most Viewed Threads";
-$l['not_enough_info_stats'] = "Sorry, but there is not enough information on this board to generate statistics. Before statistics can be generated this board needs to contain at least 1 member and 1 thread.";
-$l['replies'] = "replies";
-$l['views'] = "views";
-$l['top_referrer'] = "Top referrer: <strong>{1}</strong> (<strong>{2}</strong> referrals)";
+$l['board_stats'] = "Estatísticas do Fórum";
+$l['none'] = "Nenhum";
+$l['totals'] = "Totais";
+$l['averages'] = "Médias";
+$l['posts'] = "Postagens:";
+$l['threads'] = "Tópicos:";
+$l['members'] = "Membros:";
+$l['ppd'] = "Postagens por dia:";
+$l['tpd'] = "Tópicos por dia:";
+$l['mpd'] = "Membros por dia:";
+$l['ppm'] = "Postagens por membro:";
+$l['tpm'] = "Tópicos por membro:";
+$l['rpt'] = "Respostas por tópico:";
+$l['no_posts'] = "Não";
+$l['general'] = "Geral";
+$l['newest_member'] = "Membro Novo:";
+$l['members_posted'] = "Membros que têm postado:";
+$l['todays_top_poster'] = "Maior participação de hoje: <b>{1}</b> (<b>{2}</b> postagens)";
+$l['popular_forum'] = "Fórum mais Popular: <b>{1}</b> (<b>{2}</b> postagens, <b>{3}</b> tópicos)";
+$l['most_popular'] = "Mais Popular...";
+$l['most_replied_threads'] = "Tópicos com Mais Respostas";
+$l['most_viewed_threads'] = "Tópicos Mais Visualizados";
+$l['not_enough_info_stats'] = "Desculpe, mas não existe informação suficiente neste Fórum para gerar estatísticas. Para as estatísticas serem geradas, necessita haver pelo menos 1 membro e 1 tópico.";
+$l['replies'] = "respostas";
+$l['views'] = "visualizações";
+$l['top_referrer'] = "Maior Referência: <strong>{1}</strong> (<strong>{2}</strong> referências)";
 
