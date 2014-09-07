c3ee0d83931dc2a0097a850e45cd834f9fd79d6a
Leandro dos Santos <leandrors.sp@gmail.com>
2014-09-07 18:24:06 -0300

    2 Novos Arquivos Traduzidos
    repot.lang.php
    reputation.lang.php

--- 18/inc/languages/ptbr/report.lang.php
+++ 18/inc/languages/ptbr/report.lang.php
@@ -2,29 +2,29 @@
 /**
  * MyBB 1.8 Pacote de Idioma Português do Brasil
  * Copyright 2014 MyBB Group, All Rights Reserved
- * Traduzido por "" ""
+ * Traduzido por "Leandro dos Santos" "www.hospedagemecriacaodesites.com.br"
  */
 
-$l['report_reason'] = "Report reason:";
-$l['report_to_mod'] = "Report content to a moderator";
-$l['close_window'] = "Close Window";
+$l['report_reason'] = "Razão da Denúncia:";
+$l['report_to_mod'] = "Denunciar conteúdo a um moderador";
+$l['close_window'] = "Fechar Janela";
 
 // Content types
-$l['report_content'] = "Report Content";
-$l['report_reason_post'] = "Report Post";
-$l['report_reason_profile'] = "Report Profile";
-$l['report_reason_reputation'] = "Report Reputation";
+$l['report_content'] = "Denunciar Conteúdo";
+$l['report_reason_post'] = "Denunciar Postagem";
+$l['report_reason_profile'] = "Reportar Perfil";
+$l['report_reason_reputation'] = "Denunciar Reputação";
 
 // Content reasons
-$l['report_reason_bad'] = "Inappropriate Content";
-$l['report_reason_spam'] = "Spam Content";
-$l['report_reason_rules'] = "Breaks Forum Rules";
-$l['report_reason_wrong'] = "Wrong Forum";
-$l['report_reason_other'] = "Other";
+$l['report_reason_bad'] = "Conteúdo Impróprio";
+$l['report_reason_spam'] = "Conteúdo de Spam";
+$l['report_reason_rules'] = "Fere as Regras do Fórum";
+$l['report_reason_wrong'] = "Fórum Errado";
+$l['report_reason_other'] = "Outro";
 
-$l['success_report_voted'] = "Thanks for reporting this content.<br />A member of the forum team will check your report soon.";
+$l['success_report_voted'] = "Obrigado por denunciar este conteúdo.<br />Um membro de nossa Equipe do fórum verificará sua denúncia em breve.";
 
-$l['error_report_length'] = "Please provide a detailed reason for your report.";
-$l['error_invalid_report'] = "This content either does not exist or is not able to be reported.";
-$l['error_report_duplicate'] = "This content has already been reported by another member.<br />You may also report this content below.";
-$l['report_reason_other_description'] = "If 'Other' please provide a reason for reporting this content.";
+$l['error_report_length'] = "Por favor, forneça uma razão detalhada para sua denúncia.";
+$l['error_invalid_report'] = "Este conteúdo não existe ou não é possível de ser denunciado.";
+$l['error_report_duplicate'] = "Este conteúdo já foi denunciado por outro membro.<br />Você também pode denunciar este conteúdo abaixo.";
+$l['report_reason_other_description'] = "Se escolheu a opção 'Outro', por favor, forneça-nos uma razão pela qual está denunciando este conteúdo.";
