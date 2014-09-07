ec37e27530f38cb66f9cf3944f7f601511dba422
Leandro dos Santos <leandrors.sp@gmail.com>
2014-09-07 18:02:30 -0300

    Tradução de ratethread.lang.php concluída.

--- 18/inc/languages/ptbr/ratethread.lang.php
+++ 18/inc/languages/ptbr/ratethread.lang.php
@@ -2,19 +2,19 @@
 /**
  * MyBB 1.8 Pacote de Idioma Português do Brasil
  * Copyright 2014 MyBB Group, All Rights Reserved
- * Traduzido por "" ""
+ * Traduzido por "Leandro dos Santos" "www.hospedagemecriacaodesites.com.br"
  */
 
-$l['redirect_threadrated'] = "Thank you, the thread has been rated successfully. You will now be returned to the thread.";
+$l['redirect_threadrated'] = "Obrigado, o tópico foi avaliado com sucesso. Agora você retornará ao tópico.";
 
-$l['error_invalidrating'] = "You have selected an invalid rating for this thread. Please go back and try again.";
-$l['error_alreadyratedthread'] = "Sorry, but you have already rated this thread.";
-$l['error_cannotrateownthread'] = "Sorry, you cannot rate your own thread.";
+$l['error_invalidrating'] = "Você selecionou uma avaliação inválida para este tópico. Por favor, volte e tente novamente.";
+$l['error_alreadyratedthread'] = "Desculpe, mas você já avaliou este tópico.";
+$l['error_cannotrateownthread'] = "Desculpe, você não pode avaliar seu próprio tópico.";
 
-$l['rating_votes_average'] = "{1} Vote(s) - {2} out of 5 in Average";
-$l['one_star'] = "1 star out of 5";
-$l['two_stars'] = "2 stars out of 5";
-$l['three_stars'] = "3 stars out of 5";
-$l['four_stars'] = "4 stars out of 5";
-$l['five_stars'] = "5 stars out of 5";
-$l['rating_added'] = "Your rating has been added!";
+$l['rating_votes_average'] = "{1} Voto(s) - {2} de 5 em média";
+$l['one_star'] = "1 estrela de 5";
+$l['two_stars'] = "2 estrelas de 5";
+$l['three_stars'] = "3 estrelas de 5";
+$l['four_stars'] = "4 estrelas de 5";
+$l['five_stars'] = "5 estrelas de 5";
+$l['rating_added'] = "Sua avaliação foi adicionada!";
