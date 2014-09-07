90e83fe053fafc4168439b597523213f64cff0a1
Leandro dos Santos <leandrors.sp@gmail.com>
2014-09-07 10:44:46 -0300

    Novo arquivo atualizado.

--- 18/inc/languages/ptbr/newthread.lang.php
+++ 18/inc/languages/ptbr/newthread.lang.php
@@ -2,56 +2,56 @@
 /**
  * MyBB 1.8 Pacote de Idioma Português do Brasil
  * Copyright 2014 MyBB Group, All Rights Reserved
- * Traduzido por "" ""
+ * Traduzido por "Leandro dos Santos" "www.hospedagemecriacaodesites.com.br"
  */
 
-$l['nav_newthread'] = "New Thread";
-$l['newthread_in'] = "New Thread in {1}";
-$l['post_new_thread'] = "Post a new Thread";
-$l['thread_subject'] = "Thread Subject";
-$l['your_message'] = "Your Message:";
-$l['post_options'] = "Post Options:";
-$l['options_sig'] = "<b>Signature:</b> include your signature. (registered users only)";
-$l['options_emailnotify'] = "<b>Email Notification:</b> receive an email whenever there is a new reply. (registered users only)";
-$l['options_disablesmilies'] = "<b>Disable Smilies:</b> disable smilies from showing in this post.";
-$l['post_thread'] = "Post Thread";
-$l['preview_post'] = "Preview Post";
-$l['poll'] = "Poll:";
-$l['poll_desc'] = "Optionally you may attach a poll to this thread.";
-$l['poll_check'] = "I want to post a poll";
-$l['num_options'] = "Number of options:";
-$l['max_options'] = "(Maximum: {1})";
-$l['mod_options'] ="Moderator Options:";
-$l['close_thread'] = "<b>Close Thread</b>: prevent further posting in this thread.";
-$l['stick_thread'] = "<b>Stick Thread:</b> stick this thread to the top of the forum.";
-$l['draft_saved'] = "The new thread has successfully been saved as a draft.<br />You will now be taken to your draft listing.";
-$l['image_verification'] = "Image Verification";
-$l['verification_note'] = "Please enter the text contained within the image into the text box below it. This process is used to prevent automated posts.";
-$l['verification_subnote'] = "(case insensitive)";
-$l['invalid_captcha'] = "The image verification code that you entered was incorrect. Please enter the code exactly how it appears in the image.";
-$l['error_post_already_submitted'] = "You have already posted this thread in this forum. Please visit the forum to see your thread.";
-$l['no_prefix'] = "No Prefix";
-$l['forum_rules'] = "{1} - Rules";
+$l['nav_newthread'] = "Novo Tópico";
+$l['newthread_in'] = "Novo Tópico em {1}";
+$l['post_new_thread'] = "Postar um Novo Tópico";
+$l['thread_subject'] = "Assunto do Tópico";
+$l['your_message'] = "Sua Mensagem:";
+$l['post_options'] = "Opções de Postagem:";
+$l['options_sig'] = "<b>Assinatura:</b> inclui sua assinatura. (somente usuários registrados)";
+$l['options_emailnotify'] = "<b>Notificação por E-Mail:</b> receba um e-mail sempre que houver uma nova resposta. (somente usuários registrados)";
+$l['options_disablesmilies'] = "<b>Desativar Carinhas:</b> desativa carinhas a serem exibidas em sua postagem.";
+$l['post_thread'] = "Postar Tópico";
+$l['preview_post'] = "Pré-Visualizar Postagem";
+$l['poll'] = "Enquete:";
+$l['poll_desc'] = "Opcionalmente, você pode anexar uma enquete a este tópico.";
+$l['poll_check'] = "Eu quero postar uma enquete";
+$l['num_options'] = "Número de Alternativas:";
+$l['max_options'] = "(Máximo: {1})";
+$l['mod_options'] ="Opções de Moderador:";
+$l['close_thread'] = "<b>Fechar Tópico</b>: evitar novas postagens neste tópico.";
+$l['stick_thread'] = "<b>Fixar Tópico:</b> fixar este tópico no topo do fórum.";
+$l['draft_saved'] = "O novo tópico foi salvo como rascunho com sucesso.<br />Agora você será levado à sua lista de rascunhos.";
+$l['image_verification'] = "Imagem de Verificação";
+$l['verification_note'] = "Por favor, insira o texto contido dentro da imagem na caixa de texto abaixo. Este processo é usado para evitar mensagens automáticas.";
+$l['verification_subnote'] = "(maiúsculas e minúsculas)";
+$l['invalid_captcha'] = "O código de verificação de imagem que você informou estava incorreto. Por favor, digite o código exatamente como ele aparece na imagem.";
+$l['error_post_already_submitted'] = "Você já postou este tópico neste fórum. Por favor, visite o fórum para ver seu tópico.";
+$l['no_prefix'] = "Nenhum prefixo";
+$l['forum_rules'] = "{1} - Regras";
 
-$l['multiquote_external_one'] = "You have selected one post from another thread.";
-$l['multiquote_external'] = "You have selected {1} posts from other threads.";
-$l['multiquote_external_one_deselect'] = "deselect this post";
-$l['multiquote_external_deselect'] = "deselect these posts";
-$l['multiquote_external_one_quote'] = "Quote this post too";
-$l['multiquote_external_quote'] = "Quote these posts too";
+$l['multiquote_external_one'] = "Você selecionou uma postagem de outro tópico.";
+$l['multiquote_external'] = "Você selecionou {1} postagens de outros tópicos.";
+$l['multiquote_external_one_deselect'] = "desmarcar esta postagem";
+$l['multiquote_external_deselect'] = "desmarcar estas postagens";
+$l['multiquote_external_one_quote'] = "Citar esta postagem também";
+$l['multiquote_external_quote'] = "Citar estas postagens também";
 
-$l['redirect_newthread'] = "Thank you, your thread has been posted.";
-$l['redirect_newthread_poll'] = "<br />You will now be taken to the poll options and configuration page.";
-$l['redirect_newthread_moderation'] = "<br />The administrator has specified that all new threads require moderation. You will now be returned to the thread listing.";
-$l['redirect_newthread_thread'] = "<br />You will now be taken to the new thread.";
-$l['invalidthread'] = "The specified draft does not exist or you don't have permission to view it.";
+$l['redirect_newthread'] = "Obrigado, seu tópico foi publicado.";
+$l['redirect_newthread_poll'] = "<br />Agora você será levado à página de configuração e opções de enquete.";
+$l['redirect_newthread_moderation'] = "<br />O administrador especificou que os tópicos novos exigem moderação. Agora você será retornará à listagem de tópicos.";
+$l['redirect_newthread_thread'] = "<br />Agora você será levado para o novo tópico.";
+$l['invalidthread'] = "O rascunho espedificado não existe ou você não tem permissão para visualizá-lo.";
 
-$l['error_stop_forum_spam_spammer'] = 'Sorry, your username or IP address matches that of a known spammer. If you feel this is a mistake, please contact an administrator';
-$l['error_stop_forum_spam_fetching'] = 'Sorry, something went wrong verifying your thread against a spammer database. Most likely the database couldn\'t be accessed. Please try again later.';
+$l['error_stop_forum_spam_spammer'] = 'Desculpe, seu nome de usuário ou endereço IP corresponde ao de um spammer conhecido. Se você considera isso um erro, entre em contato com o administrador';
+$l['error_stop_forum_spam_fetching'] = 'Desculpe, algo deu errado, verificando seu tópico contra um banco de dados de spammers. Provavelmente o banco de dados não pode ser acessado. Por favor, tente mais tarde.';
 
-$l['error_suspendedposting'] = "Your posting privileges are currently suspended {1}.<br /><br />
+$l['error_suspendedposting'] = "Seus privilégios de postagem no momento estão suspensos {1}.<br /><br />
 
-Suspension Date: {2}";
-$l['error_suspendedposting_temporal'] = "until {1}";
-$l['error_suspendedposting_permanent'] = "permanently";
+Data de Suspensão: {2}";
+$l['error_suspendedposting_temporal'] = "até {1}";
+$l['error_suspendedposting_permanent'] = "permanentemente";
 
