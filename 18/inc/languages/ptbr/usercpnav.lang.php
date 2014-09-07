de935427aee031ed78186ea92e621716bb5a4888
Leandro dos Santos <leandrors.sp@gmail.com>
2014-09-07 19:39:37 -0300

    Novos Arquivos Traduzidos...

--- 18/inc/languages/ptbr/usercpnav.lang.php
+++ 18/inc/languages/ptbr/usercpnav.lang.php
@@ -2,32 +2,32 @@
 /**
  * MyBB 1.8 Pacote de Idioma Português do Brasil
  * Copyright 2014 MyBB Group, All Rights Reserved
- * Traduzido por "" ""
+ * Traduzido por "Leandro dos Santos" "www.hospedagemecriacaodesites.com.br"
  */
 
 $l['ucp_nav_width'] = "180";
 $l['ucp_nav_menu'] = "Menu";
-$l['ucp_nav_messenger'] = "Messenger";
-$l['ucp_nav_compose'] = "Compose";
-$l['ucp_nav_tracking'] = "Tracking";
-$l['ucp_nav_edit_folders'] = "Edit Folders";
-$l['ucp_nav_profile'] = "Your Profile";
-$l['ucp_nav_edit_profile'] = "Edit Profile";
-$l['ucp_nav_edit_options'] = "Edit Options";
-$l['ucp_nav_change_email'] = "Change Email";
-$l['ucp_nav_change_pass'] = "Change Password";
-$l['ucp_nav_change_username'] = "Change Username";
-$l['ucp_nav_edit_sig'] = "Change Signature";
-$l['ucp_nav_change_avatar'] = "Change Avatar";
-$l['ucp_nav_misc'] = "Miscellaneous";
-$l['ucp_nav_editlists'] = "Buddy/Ignore List";
-$l['ucp_nav_favorite_threads'] = "Favorite Threads";
-$l['ucp_nav_subscribed_threads'] = "Subscribed Threads";
-$l['ucp_nav_forum_subscriptions'] = "Forum Subscriptions";
-$l['ucp_nav_drafts'] = "Saved Drafts";
-$l['ucp_nav_drafts_active'] = "<strong>Saved Drafts ({1})</strong>";
-$l['ucp_nav_notepad'] = "Personal Pad";
-$l['ucp_nav_view_profile'] = "View Profile";
-$l['ucp_nav_home'] = "User CP Home";
-$l['ucp_nav_usergroups'] = "Group Memberships";
-$l['ucp_nav_attachments'] = "Manage Attachments";
+$l['ucp_nav_messenger'] = "Mensageiro";
+$l['ucp_nav_compose'] = "Nova Mensagem";
+$l['ucp_nav_tracking'] = "Rastreamento";
+$l['ucp_nav_edit_folders'] = "Editar Pastas";
+$l['ucp_nav_profile'] = "Seu Perfil";
+$l['ucp_nav_edit_profile'] = "Editar Perfil";
+$l['ucp_nav_edit_options'] = "Editar Opções";
+$l['ucp_nav_change_email'] = "Alterar E-Mail";
+$l['ucp_nav_change_pass'] = "Alterar Senha";
+$l['ucp_nav_change_username'] = "Alterar Nome de Usuário";
+$l['ucp_nav_edit_sig'] = "Alterar Assinatura";
+$l['ucp_nav_change_avatar'] = "Alterar Avatar";
+$l['ucp_nav_misc'] = "Outras Opções";
+$l['ucp_nav_editlists'] = "Lista de Amigos / Ignorados";
+$l['ucp_nav_favorite_threads'] = "Tópicos Favoritos";
+$l['ucp_nav_subscribed_threads'] = "Tópicos Subscritos";
+$l['ucp_nav_forum_subscriptions'] = "Fóruns Subscritos";
+$l['ucp_nav_drafts'] = "Rascunhos Salvos";
+$l['ucp_nav_drafts_active'] = "<strong>Rascunhos Salvos ({1})</strong>";
+$l['ucp_nav_notepad'] = "Bloco de Notas";
+$l['ucp_nav_view_profile'] = "Visualizar Perfil";
+$l['ucp_nav_home'] = "Painel de Controle de Usuário";
+$l['ucp_nav_usergroups'] = "Grupos de Membros";
+$l['ucp_nav_attachments'] = "Gerenciar Anexos";
