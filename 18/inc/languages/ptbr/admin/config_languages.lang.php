1cf335c69a8121836012a9dd32bc6a348e008b12
dthiago <thiagoalves@live.com>
2014-09-22 04:48:19 -0300

    Update config_languages.lang.php

--- 18/inc/languages/ptbr/admin/config_languages.lang.php
+++ 18/inc/languages/ptbr/admin/config_languages.lang.php
@@ -1,57 +1,58 @@
 <?php
 /**
- * MyBB 1.8 English Language Pack
- * Copyright 2014 MyBB Group, All Rights Reserved
- *
+ **
+ * MyBB 1.8 Pacote de Língua Brasileira
+ * Direitos Autorais 2014 MyBB Group, Todos os Direitos Reservados
+ * Traduzido por dthiago "ArnoldLayne-xXx" http://portal.bf4brasil.com.br/
  */
 
-$l['languages'] = "Languages";
-$l['languages_desc'] = "The language packs you have installed on your forum are shown below. You can choose to edit the language files, edit the files with another language, or edit the language's properties.";
-$l['find_language_packs'] = "Find Language Packs";
-$l['language_files'] = "Language Files";
-$l['language_files_desc'] = "Below are the files within the selected language, for both the front-end and the admin control panel. Here you can choose to edit these files.";
-$l['quick_phrases'] = "Quick Phrases";
-$l['quick_phrases_desc'] = "Quick access to commonly modified language phrases.";
+$l['languages'] = "Idiomas";
+$l['languages_desc'] = "Os pacotes de idiomas que instalou no seu fórum encontram-se visíveis abaixo. Você pode escolher editar os arquivos do idioma, editar arquivos em outro idioma, ou editar as propriedades do idioma.";
+$l['find_language_packs'] = "Procurar Pacotes de Idioma";
+$l['language_files'] = "Arquivos de Idioma";
+$l['language_files_desc'] = "Abaixo estão os arquivos dentro do Idioma selecionados, do Fórum e do Painel de Controle da Administração. Aqui pode escolher editar estes arquivos.";
+$l['quick_phrases'] = "Acesso rápido a frases";
+$l['quick_phrases_desc'] = "Acesso rápido à modificação do idioma das frases.";
 
-$l['edit_with'] = "Edit with {1}";
-$l['edit_language_variables'] = "Edit Language Variables";
-$l['edit_language_variables_desc'] = "Here you can edit the different language strings to your liking.";
-$l['edit_properties_desc'] = "Here you can edit a language pack properties.";
-$l['installed_language_packs'] = "Installed Language Packs";
-$l['front_end'] = "Front End";
+$l['edit_with'] = "Editar {1}";
+$l['edit_language_variables'] = "Editar variáveis do idioma";
+$l['edit_language_variables_desc'] = "Aqui você pode editar um idioma diferente ao seu estilo para o seu fórum.";
+$l['edit_properties_desc'] = "Aqui você pode editar as propriedades do pacote de idioma.";
+$l['installed_language_packs'] = "Pacotes de Idiomas Instalados";
+$l['front_end'] = "Fórum";
 $l['admin_cp'] = "Admin CP";
-$l['save_language_file'] = "Save Language File";
-$l['nav_editing_set'] = "Editing Language Set Properties";
-$l['edit_properties'] = "Edit Language Pack Properties";
-$l['friendly_name'] = "Friendly Name";
-$l['language_in_html'] = "Language in &lt;html&gt; tag";
-$l['charset'] = "Character Set";
-$l['admin'] = "Contains Admin CP language variables?";
-$l['rtl'] = "Right-To-Left?";
+$l['save_language_file'] = "Salvar Aqruivos de Idioma";
+$l['nav_editing_set'] = "Editar a configuração das Propriedades do idioma";
+$l['edit_properties'] = "Editar Propriedades do pacote do idioma";
+$l['friendly_name'] = "Nome amigável";
+$l['language_in_html'] = "Idioma do tag em html ";
+$l['charset'] = "Configuração do caractere";
+$l['admin'] = "Contém variáveis do Admin CP?";
+$l['rtl'] = "Direita-para-Esquerda?";
 
-$l['quickphrases_agreement'] = 'Registration Agreement - Title';
-$l['quickphrases_agreement_1'] = 'Registration Agreement - Paragraph 1';
-$l['quickphrases_agreement_2'] = 'Registration Agreement - Paragraph 2';
-$l['quickphrases_agreement_3'] = 'Registration Agreement - Paragraph 3';
-$l['quickphrases_agreement_4'] = 'Registration Agreement - Paragraph 4';
-$l['quickphrases_agreement_5'] = 'Registration Agreement - Paragraph 5';
+$l['quickphrases_agreement'] = 'Termos do Registro - Título';
+$l['quickphrases_agreement_1'] = 'Termos do Registro - Parágrafo 1';
+$l['quickphrases_agreement_2'] = 'Termos do Registro - Parágrafo 2';
+$l['quickphrases_agreement_3'] = 'Termos do Registro - Parágrafo 3';
+$l['quickphrases_agreement_4'] = 'Termos do Registro - Parágrafo 4';
+$l['quickphrases_agreement_5'] = 'Termos do Registro - Parágrafo 5';
 
-$l['quickphrases_error_nopermission_guest_1'] = 'No Permissions Message - Paragraph 1';
-$l['quickphrases_error_nopermission_guest_2'] = 'No Permissions Message - Paragraph 2';
-$l['quickphrases_error_nopermission_guest_3'] = 'No Permissions Message - Paragraph 3';
-$l['quickphrases_error_nopermission_guest_4'] = 'No Permissions Message - Paragraph 4';
+$l['quickphrases_error_nopermission_guest_1'] = 'Sem Permissão - Parágrafo 1';
+$l['quickphrases_error_nopermission_guest_2'] = 'Sem Permissão - Parágrafo 2';
+$l['quickphrases_error_nopermission_guest_3'] = 'Sem Permissão - Parágrafo 3';
+$l['quickphrases_error_nopermission_guest_4'] = 'Sem Permissão - Parágrafo 4';
 
-$l['no_languages'] = "No Languages";
-$l['no_language_files_front_end'] = "There are no Front End Language files found.";
-$l['no_language_files_admin_cp'] = "There are no Admin CP Language files found.";
+$l['no_languages'] = "Nenhum Idioma";
+$l['no_language_files_front_end'] = "Não foram encontrados arquivos de Idioma no Fórum.";
+$l['no_language_files_admin_cp'] = "Não foram encontrados arquivod de Idioma do Admin CP.";
 
-$l['error_invalid_set'] = "Invalid language set specified.";
-$l['error_invalid_file'] = "Invalid language file specified.";
-$l['error_cannot_write_to_file'] = "Cannot write to file.  Check the file permissions for this language set and try again";
+$l['error_invalid_set'] = "Idioma especificado inválido.";
+$l['error_invalid_file'] = "Arqucios de idioma especificado inválido.";
+$l['error_cannot_write_to_file'] = "Não é possível salvar o arquivo.  Verifique as permissões para este pacote de Idioma e tente novamente";
 
-$l['alert_note_cannot_write'] = "Before you can edit the language file, you must CHMOD the files in this language set so that the server has permission to write to them.";
+$l['alert_note_cannot_write'] = "Antes que possa editar o arquico de idioma, você tem de ajustar as permissões (CHMOD) dos arquivos deste pacote de idioma, para que o servidor consiga alterá-los.";
 
-$l['success_langfile_updated'] = "The language file has been updated successfully.";
-$l['success_langprops_updated'] = "The language properties have been updated successfully.";
-$l['success_quickphrases_updated'] = "The quick language phrases have been updated successfully.";
+$l['success_langfile_updated'] = "O arquivos de idioma foi atualizado com sucesso.";
+$l['success_langprops_updated'] = "As propriedades do Idioma foram atualizadas com sucesso.";
+$l['success_quickphrases_updated'] = "As frases do Idioma rápido foram atualizados com sucesso.";
 
