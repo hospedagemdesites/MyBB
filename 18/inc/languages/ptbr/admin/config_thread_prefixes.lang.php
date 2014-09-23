ad3a0ed3132c4828dfd0465d477a517e2796d1c9
dthiago <thiagoalves@live.com>
2014-09-23 19:38:40 -0300

    Update config_thread_prefixes.lang.php

--- 18/inc/languages/ptbr/admin/config_thread_prefixes.lang.php
+++ 18/inc/languages/ptbr/admin/config_thread_prefixes.lang.php
@@ -1,42 +1,42 @@
 <?php
 /**
- * MyBB 1.8 Pacote de Idioma Português do Brasil
- * Copyright 2014 MyBB Group, All Rights Reserved
- *
+ * MyBB 1.8 Pacote de Língua Brasileira
+ * Direitos Autorais 2014 MyBB Group, Todos os Direitos Reservados
+ * Traduzido por dthiago ArnoldLayne-xXx http://bf4brasil.com.br/
  */
 
-$l['thread_prefixes'] = "Thread Prefixes";
-$l['thread_prefixes_desc'] = "Thread prefixes allow you to define prefixes that users can assign to their threads. Threads can then be filtered by prefix within a forum.";
+$l['thread_prefixes'] = "Prefixos de Tópico";
+$l['thread_prefixes_desc'] = "Prefixos de tópico permitem definir prefixos em que os usuários podem adicionar aos tópicos. Os tópicos podem ser filtrados por prefixo .";
 
-$l['add_new_thread_prefix'] = "Add New Thread Prefix";
-$l['add_new_thread_prefix_desc'] = "Here you can add a new thread prefix and define where it is available, as well as which usergroups can use it.";
+$l['add_new_thread_prefix'] = "Adicionar Novo Prefixo de Tópico";
+$l['add_new_thread_prefix_desc'] = "Aqui é possível adicionar um novo prefixo e definir onde ele ficará disponível assim como os grupos que podem usá-lo.";
 
-$l['edit_prefix'] = "Edit Prefix";
-$l['edit_prefix_desc'] = "Here you can edit a thread prefix and change how it is display, where it is available and which usergroups can use it.";
-$l['edit_thread_prefix'] = "Edit Thread Prefix";
-$l['delete_thread_prefix'] = "Delete Thread Prefix";
+$l['edit_prefix'] = "Editar Prefixo";
+$l['edit_prefix_desc'] = "Aqui é possível editar um prefixo de um tópico e modificar como a maneira de como será visualizado, onde estará disponível e quais os grupos que poderão usá-lo.";
+$l['edit_thread_prefix'] = "Editar Prefixo de Tópico";
+$l['delete_thread_prefix'] = "Excluir Prefixo de Tópico";
 
-$l['prefix_options'] = "Prefix Options";
-$l['save_thread_prefix'] = "Save Thread Prefix";
+$l['prefix_options'] = "Opções de Prefixo";
+$l['save_thread_prefix'] = "Salvar Prefixo de Tópico";
 
-$l['prefix'] = "Prefix";
-$l['prefix_desc'] = "Plain text version of the prefix to be displayed in selection menus.";
-$l['display_style'] = "Display Style";
-$l['display_style_desc'] = "This is how the prefix will appear next to thread subjects. You may enter HTML markup here or simply use the same as the plain text version above.";
-$l['available_in_forums'] = "Available in forums";
-$l['available_to_groups'] = "Available to groups";
+$l['prefix'] = "Prefixo";
+$l['prefix_desc'] = "Texto do prefixo a ser mostrado na selecção dos menus.";
+$l['display_style'] = "Exibição do Estilo";
+$l['display_style_desc'] = "Isto é como o prefixo irão aparecer junto ao assunto do tópico. É possível inserir HTML à formatação do prefixo ou usar o mesmo texto do prefixo definido acima.";
+$l['available_in_forums'] = "Disponível nos fóruns";
+$l['available_to_groups'] = "Disponível nos grupos";
 
-$l['no_thread_prefixes'] = "There are no thread prefixes setup on your forum.";
+$l['no_thread_prefixes'] = "Não existem prefixos de tópicos configurados no seu fórum.";
 
-$l['confirm_thread_prefix_deletion'] = "Are you sure you want to delete this thread prefix? Note: You should remember to update any custom moderator tools which use this prefix after deletion.";
+$l['confirm_thread_prefix_deletion'] = "Tem a certeza que deseja a exclusão  deste prefixo de tópico? Observação: Não se pode esquecer de atualizar qualquer ferramenta personalizada que use este prefixo após exclusão.";
 
-$l['success_thread_prefix_created'] = "The thread prefix has been created successfully.";
-$l['success_thread_prefix_updated'] = "The thread prefix has been updated successfully.";
-$l['success_thread_prefix_deleted'] = "The thread prefix has been deleted successfully. Please update any custom moderator tools which use this prefix";
+$l['success_thread_prefix_created'] = "O prefixo do tópico foi criado com sucesso.";
+$l['success_thread_prefix_updated'] = "O prefixo do tópico foi atualizado com sucesso.";
+$l['success_thread_prefix_deleted'] = "O prefixo do tópico foi excluído com sucesso. Por favor atualize qualquer ferramenta personalizada de moderação que use este prefixo";
 
-$l['error_missing_prefix'] = "Please enter the prefix you wish to add.";
-$l['error_missing_display_style'] = "Please enter a display style for this prefix.";
-$l['error_no_forums_selected'] = "Please select the forums in which this prefix will be available.";
-$l['error_no_groups_selected'] = "Please select the groups to which this prefix will be available.";
-$l['error_invalid_prefix'] = "The specified thread prefix does not exist.";
+$l['error_missing_prefix'] = "Por favor insira o prefixo que deseja adicionar.";
+$l['error_missing_display_style'] = "Por favor insira a exibição do estilo para este prefixo.";
+$l['error_no_forums_selected'] = "Por favor selecione os fóruns  onde deseja disponibilizar este prefixo.";
+$l['error_no_groups_selected'] = "Por favor seleccione  os grupos onde deseja disponibilizar este prefixo.";
+$l['error_invalid_prefix'] = "O prefixo do tópico especificado não existe.";
 
