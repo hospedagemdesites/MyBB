a535e0799b628d1a343f85355d3e1a8bc1076502
dthiago <thiagoalves@live.com>
2014-09-23 19:26:34 -0300

    Update config_smilies.lang.php

--- 18/inc/languages/ptbr/admin/config_smilies.lang.php
+++ 18/inc/languages/ptbr/admin/config_smilies.lang.php
@@ -1,64 +1,64 @@
 <?php
 /**
- * MyBB 1.8 Pacote de Idioma Português do Brasil
- * Copyright 2014 MyBB Group, All Rights Reserved
- *
+ * MyBB 1.8 Pacote de Língua Brasileira
+ * Direitos Autorais 2014 MyBB Group, Todos os Direitos Reservados
+ * Traduzido por dthiago ArnoldLayne-xXx http://bf4brasil.com.br/
  */
 
-$l['smilies'] = "Smilies";
-$l['manage_smilies'] = "Manage Smilies";
-$l['manage_smilies_desc'] = "This section allows you to edit and delete your smilies.";
-$l['add_smilie'] = "Add New Smilie";
-$l['add_smilie_desc'] = "Here you can add a single new smilie.";
-$l['add_multiple_smilies'] = "Add Multiple Smilies";
-$l['add_multiple_smilies_desc'] = "Here you can add multiple new smilies at once.";
-$l['edit_smilie'] = "Edit Smilie";
-$l['edit_smilie_desc'] = "Here you can edit a single smilie.";
-$l['mass_edit'] = "Mass Edit";
-$l['mass_edit_desc'] = "Here you can easily edit all your smilies in one go.";
+$l['smilies'] = "Sorrisos";
+$l['manage_smilies'] = "Gerenciados de Sorrisos";
+$l['manage_smilies_desc'] = "Aqui você pode editar e apagar os seus sorrios.";
+$l['add_smilie'] = "Adicio Novo Sorriso";
+$l['add_smilie_desc'] = "Aqui pode adicionar um único novo sorriso.";
+$l['add_multiple_smilies'] = "Adicionar Múltiplos Sorrisos";
+$l['add_multiple_smilies_desc'] = "Aqui você pode adicionar múltiplos sorrisos de uma só vez.";
+$l['edit_smilie'] = "Editar Sorrisos";
+$l['edit_smilie_desc'] = "Aqui pode editar um sorriso.";
+$l['mass_edit'] = "Edição em Massa";
+$l['mass_edit_desc'] = "Aqui você pode editar todos os sorrisos de uma só vez.";
 
-$l['no_smilies'] = "There are no smilies on your forum at this time.";
+$l['no_smilies'] = "Não existem sorrisos no seu fórum neste momento.";
 
-$l['image'] = "Image";
-$l['name'] = "Name";
-$l['text_replace'] = "Text to Replace";
-$l['text_replace_desc'] = "This is the text to replace with the smilie (enter one per line).";
-$l['image_path'] = "Image Path";
-$l['image_path_desc'] = "This is the path to the smilie image. If you want to use different smilie images for different themes, please use <strong>{theme}</strong> to represent the image directory of each theme.";
-$l['order'] = "Order";
-$l['display_order'] = "Display Order";
-$l['display_order_desc'] = "The order on the smilies list that this will appear. This number should not be the same as another smilie's.";
-$l['mass_edit_show_clickable'] = "Show on Clickable?";
-$l['show_clickable'] = "Show on clickable list?";
-$l['show_clickable_desc'] = "Do you want this smilie to show on the clickable smilie list on the post editor?";
-$l['include'] = "Add?";
-$l['path_to_images'] = "Path to Images";
-$l['path_to_images_desc'] = "This is the path to the folder that the images are in.";
-$l['smilie_delete'] = "Delete?";
-$l['save_smilie'] = "Save Smilie";
-$l['save_smilies'] = "Save Smilies";
-$l['show_smilies'] = "Show Smilies";
-$l['reset'] = "Reset";
+$l['image'] = "Imagem";
+$l['name'] = "Nome";
+$l['text_replace'] = "Texto Substituto";
+$l['text_replace_desc'] = "Este é o texto a ser substituído pelo Sorriso (insira um por linha).";
+$l['image_path'] = "Caminho da Imagem";
+$l['image_path_desc'] = "Este é o caminho para a imagem do sorriso.";
+$l['order'] = "Ordem";
+$l['display_order'] = "Ordem de Exibição";
+$l['display_order_desc'] = "A ordenação da lista de sorrisos vai aparecer não pode ser igual a outros sorrisos.";
+$l['mass_edit_show_clickable'] = "Mostrar como clicável?";
+$l['show_clickable'] = "Mostrar como lista de clicáveis?";
+$l['show_clickable_desc'] = "Quer que este sorriso seja visualizado na lista de sorrisos clicáveis no editor de mensagem?";
+$l['include'] = "Adicionar?";
+$l['path_to_images'] = "Caminho para as imagens";
+$l['path_to_images_desc'] = "Este é o caminho para a pasta onde as imagens estão.";
+$l['smilie_delete'] = "Apagar?";
+$l['save_smilie'] = "Salvar Sorriso";
+$l['save_smilies'] = "Salvar Sorrisos";
+$l['show_smilies'] = "Mostrar Sorrisos";
+$l['reset'] = "Reiniciar";
 
-$l['error_missing_name'] = "You did not enter a name for this smilie.";
-$l['error_missing_text_replacement'] = "You did not enter a text replacement for this smilie.";
-$l['error_missing_path'] = "You did not enter a path for this smilie.";
-$l['error_missing_path_multiple'] = "You did not enter a path.";
-$l['error_missing_order'] = "You did not enter a display order for this smilie.";
-$l['error_duplicate_order'] = "You did not enter a valid display order for this smilie.";
-$l['error_missing_clickable'] = "You did not specify yes or no for the \"Show Clickable\" option.";
-$l['error_no_smilies'] = "There are no smilies in the specified directory, or all smilies in the directory have already been added.";
-$l['error_no_images'] = "There are no images in the specified directory.";
-$l['error_none_included'] = "You did not select any smilies to include.";
-$l['error_invalid_path'] = "You did not enter a valid path.";
-$l['error_invalid_smilie'] = "The specified smilie does not exist.";
+$l['error_missing_name'] = "Você não inseriu um nome para este sorriso";
+$l['error_missing_text_replacement'] = "Você não inseriu um texto de substituição para este sorriso";
+$l['error_missing_path'] = "Você não inseriu um caminho para este sorriso";
+$l['error_missing_path_multiple'] = "Você não inseriu um caminho";
+$l['error_missing_order'] = "Você não inseriu uma ordem de exibição para este sorriso";
+$l['error_duplicate_order'] = "Você não inseriu uma ordem de exibição válida para este Sorriso.";
+$l['error_missing_clickable'] = "Você não especificou se sim ou se não para a opção \"Clicar Exibir\".";
+$l['error_no_smilies'] = "Não existem sorrisos na pasta especificada ou todos os sorrisos na pasta  foram adicionados.";
+$l['error_no_images'] = "Não existem imagens na pasta especificada";
+$l['error_none_included'] = "Você não selecionou sorrisos para incluir.";
+$l['error_invalid_path'] = "Você não inseriu um caminho válido";
+$l['error_invalid_smilie'] = "O sorriso especificado não existe.";
 
-$l['success_smilie_added'] = "The smilie has been added successfully.";
-$l['success_multiple_smilies_added'] = "The selected smilies have been added successfully.";
-$l['success_smilie_updated'] = "The smilie has been updated successfully.";
-$l['success_multiple_smilies_updated'] = "The smilies have been updated successfully.";
-$l['success_smilie_deleted'] = "The selected smilie has been deleted successfully.";
-$l['success_mass_edit_updated'] = "The smilies have been updated successfully.";
+$l['success_smilie_added'] = "O sorriso foi adicionado com sucesso.";
+$l['success_multiple_smilies_added'] = "Os sorrisos selecionados foram adicionados com sucesso.";
+$l['success_smilie_updated'] = "O sorriso foi atualizado com sucesso.";
+$l['success_multiple_smilies_updated'] = "Os sorrisos foram atualizados com sucesso.";
+$l['success_smilie_deleted'] = "O sorriso selecionado foi apagado com sucesso.";
+$l['success_mass_edit_updated'] = "Os sorrisos foram atualizados com sucesso.";
 
-$l['confirm_smilie_deletion'] = "Are you sure you wish to delete this smilie?";
+$l['confirm_smilie_deletion'] = "Tem a certeza que quer apagar este sorriso?";
 
