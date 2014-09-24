59692d909123cda7a735d1a47b35312830912248
NewtonPDL <modnewton@gmail.com>
2014-09-23 22:13:16 -0300

    Update announcements.lang.php
    Anúncios = Comunicado como proposto, trocado "não é válido" por "inválido" para melhor definição.

--- 18/inc/languages/ptbr/announcements.lang.php
+++ 18/inc/languages/ptbr/announcements.lang.php
@@ -2,15 +2,16 @@
 /**
  * MyBB 1.8 Pacote de Idioma Português do Brasil
  * Copyright 2014 MyBB Group, All Rights Reserved
- * Traduzido por "Leandro dos Santos" "www.hospedagemecriacaodesites.com.br"
+ * Traduzido por "Leandro dos "Santos" "www.hospedagemecriacaodesites.com.br"
+ * Revisado por "NewtonPDL" "http://community.mybb.com/user-35908.html"
  */
 
-$l['nav_announcements'] = "Anúncios do fórum";
-$l['announcements'] = "Anúncio";
-$l['forum_announcement'] = "Anúncios do fórum: {1}";
-$l['error_invalidannouncement'] = "O anúncio específico não é válido.";
+$l['nav_announcements'] = "Comunicados do fórum";
+$l['announcements'] = "Comunicado";
+$l['forum_announcement'] = "Comunicados do fórum: {1}";
+$l['error_invalidannouncement'] = "O comunicado específico é inválido.";
 
-$l['announcement_edit'] = "Editar este anúncio";
-$l['announcement_qdelete'] = "Excluir este anúncio";
-$l['announcement_quickdelete_confirm'] = "Tem certeza que deseja excluir este anúncio?";
+$l['announcement_edit'] = "Editar este comunicado";
+$l['announcement_qdelete'] = "Excluir este comunicado";
+$l['announcement_quickdelete_confirm'] = "Tem certeza que deseja excluir este comunicado?";
 
