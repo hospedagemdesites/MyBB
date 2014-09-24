df37853cc9f037f06bb32d33ff7186b315b2cde2
NewtonPDL <modnewton@gmail.com>
2014-09-24 18:42:36 -0300

    Update archive.lang.php
    Corrigido erro de digitação. Alterada frase de registro para melhor compreensão dos usuários.

--- 18/inc/languages/ptbr/archive.lang.php
+++ 18/inc/languages/ptbr/archive.lang.php
@@ -3,15 +3,16 @@
  * MyBB 1.8 Pacote de Idioma Português do Brasil
  * Copyright 2014 MyBB Group, All Rights Reserved
  * Traduzido por "Leandro dos Santos" "www.hospedagemecriacaodesites.com.br"
+ * Revisado por "NewtonPDL" "http://community.mybb.com/user-35908.html"
  */
 
 $l['archive_fullversion'] = "Versão completa:";
 $l['archive_replies'] = "Respostas";
 $l['archive_reply'] = "Resposta";
 $l['archive_pages'] = "Páginas:";
-$l['archive_note'] = "Você está vendo uma versão siplificada do nosso conteúdo. <a href=\"{1}\">Ver versão completa</a> com a formatação apropriada.";
-$l['archive_nopermission'] = "Desculpe, que você não tem permissão para acessar este recurso.";
+$l['archive_note'] = "Você está visualizando uma versão simplificada de nosso conteúdo. <a href=\"{1}\">Ver versão completa</a> com a formatação apropriada.";
+$l['archive_nopermission'] = "Desculpe, você não tem permissão para acessar este recurso.";
 $l['error_nothreads'] = "Atualmente não há tópicos neste fórum.";
 $l['error_unapproved_thread'] = "Este tópico não está aprovado. Por favor, <a href=\"{1}\">consulte a versão completa</a> para ver o conteúdo deste tópico.";
 $l['archive_not_found'] = "A página solicitada não foi encontrada neste servidor.";
-$l['error_mustlogin'] = "Este fórum de discussão exige que todos os usuários de identifiquem-se.";
+$l['error_mustlogin'] = "Este fórum exige que todos os usuários de utilizem uma conta, registre-se.";
