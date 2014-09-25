33932d66173e2520da707c866a39d66a7c5e240f
dthiago <thiagoalves@live.com>
2014-09-25 02:10:16 -0300

    Update tools_maillogs.lang.php

--- 18/inc/languages/ptbr/admin/tools_maillogs.lang.php
+++ 18/inc/languages/ptbr/admin/tools_maillogs.lang.php
@@ -1,37 +1,37 @@
 <?php
 /**
- * MyBB 1.8 Pacote de Idioma Português do Brasil
- * Copyright 2014 MyBB Group, All Rights Reserved
- *
+ * MyBB 1.8 Pacote de Língua Brasileira
+ * Direitos Autorais 2014 MyBB Group, Todos os Direitos Reservados
+ * Traduzido por dthiago ArnoldLayne-xXx http://bf4brasil.com.br/
  */
 
-$l['user_email_log'] = "User Email Log";
-$l['user_email_log_desc'] = "All emails sent from one member to another member as well as emails sent by the 'Send Thread to a Friend' feature are logged and shown below. Here you can identify potential abusers of this function.";
-$l['prune_user_email_log'] = "Prune User Email Log";
+$l['user_email_log'] = "Relatório de Email de Usuário";
+$l['user_email_log_desc'] = "Todos os emails enviados de um membro para outro assim como os emails enviados pela opção 'Enviar Tópico a um Amigo' são registrados e visualizados abaixo. Aqui você pode identificar potenciais abusadores desta função.";
+$l['prune_user_email_log'] = "Excluir Relatório de Email de usuário";
 
-$l['close_window'] = "Close Window";
-$l['user_email_log_viewer'] = "User Email Log Viewer";
-$l['to'] = "To";
-$l['from'] = "From";
-$l['ip_address'] = "IP Address";
-$l['subject'] = "Subject";
-$l['date'] = "Date";
+$l['close_window'] = "Fechar Janela";
+$l['user_email_log_viewer'] = "Visualizador de relatórios de Email";
+$l['to'] = "Para";
+$l['from'] = "De";
+$l['ip_address'] = "Endereço IP";
+$l['subject'] = "Assunto";
+$l['date'] = "Data";
 $l['email'] = "Email";
-$l['date_sent'] = "Date Sent";
-$l['deleted'] = "Deleted";
-$l['sent_using_send_thread_feature'] = "Sent using the Send Thread to Friend feature";
-$l['thread'] = "Thread:";
-$l['find_emails_by_user'] = "Find all emails sent by this user";
-$l['find'] = "Find";
-$l['deleted_user'] = "Deleted User";
-$l['email_sent_to_user'] = "Email sent to user";
-$l['email_sent_using_contact_form'] = "Email sent using the contact form";
-$l['no_logs'] = "There are no log entries with the selected criteria.";
-$l['filter_user_email_log'] = "Filter User Email Log";
-$l['username_is'] = "Username is";
-$l['email_contains'] = "Email Address contains";
-$l['subject_contains'] = "Subject contains";
-$l['find_emails_to_user'] = "Find all emails sent to this user";
+$l['date_sent'] = "Data de Envio";
+$l['deleted'] = "Excluído";
+$l['sent_using_send_thread_feature'] = "Enviar utilizando a opção Enviar Tópico a um Amigo";
+$l['thread'] = "Tópico:";
+$l['find_emails_by_user'] = "Procurar todos os emails enviados por este usuário";
+$l['find'] = "Procurar";
+$l['deleted_user'] = "Usuário Excluído";
+$l['email_sent_to_user'] = "Email enviado ao usuário";
+$l['email_sent_using_contact_form'] = "Email enviado através do formulário de contato";
+$l['no_logs'] = "Não existem entradas no relatório de acordo com o critério selecionado.";
+$l['filter_user_email_log'] = "Filtrar relatório de Email de usuárioes";
+$l['username_is'] = "O Nome de usuário é";
+$l['email_contains'] = "O Endereço de Email contém";
+$l['subject_contains'] = "O Assunto contém";
+$l['find_emails_to_user'] = "Procurar todos os emails enviados a este usuário";
 
-$l['error_invalid_user'] = "The username you entered does not exist.";
+$l['error_invalid_user'] = "O nome de usuário que inseriu não existe.";
 
