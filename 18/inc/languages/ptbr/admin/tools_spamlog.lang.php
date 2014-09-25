e237cad7906c40e7ec1bbaa7c8a2a25ec405c1f6
dthiago <thiagoalves@live.com>
2014-09-24 21:56:38 -0300

    Update tools_spamlog.lang.php

--- 18/inc/languages/ptbr/admin/tools_spamlog.lang.php
+++ 18/inc/languages/ptbr/admin/tools_spamlog.lang.php
@@ -1,31 +1,31 @@
 <?php
 /**
- * MyBB 1.8 Pacote de Idioma Português do Brasil
- * Copyright 2014 MyBB Group, All Rights Reserved
+ * MyBB 1.8 Pacote de Língua Brasileira
+ * Direitos Autorais 2014 MyBB Group, Todos os Direitos Reservados
+ * Traduzido por dthiago ArnoldLayne-xXx http://bf4brasil.com.br/
  */
 
+$l['spam_logs']       = 'Relatórios de Spam';
+$l['spam_logs_desc']  = 'Esta seção permite-lhe visualizar o historico de usuários bloqueados pelos filtros de spam.';
+$l['prune_spam_logs']       = 'Limpar relatórios de spam';
+$l['prune_spam_logs_desc']  = 'Aqui você pode limpar os relatórios de spam e de acordo com um critério especificado.';
+$l['spam_username']   = 'Nome de usuário';
+$l['spam_email']      = 'Endereço Email';
+$l['spam_ip']         = 'Endereço IP';
+$l['spam_date']       = 'Data';
+$l['spam_confidence'] = 'Confiança';
+$l['no_spam_logs'] = 'Ainda não foram encontrados usuários bloqueados pelos filtros de spam.';
+$l['success_pruned_spam_logs'] = 'Os relatórios de spam foram limpos com sucesso.';
+$l['note_logs_locked'] = 'Por razões de segurança os relatórios com menos de 24 horas não podem ser limpos.';
+$l['all_usernames'] = 'Todos os nomes de usuário';
+$l['all_emails'] = ' Todos os emails de usuário';
+$l['date_range'] = "Data:";
+$l['days'] = "dias";
+$l['filter_spam_logs'] = 'Filtro de Relatórios Spam';
+$l['asc'] = "Crescente";
+$l['desc'] = "Decrescente";
 
-$l['spam_logs']       = 'Spam Logs';
-$l['spam_logs_desc']  = 'This section allows you to view a history of users blocked by the spam filters.';
-$l['prune_spam_logs']       = 'Prune Spam Logs';
-$l['prune_spam_logs_desc']  = 'Here you can prune the spam logs matching a specified criteria.';
-$l['spam_username']   = 'Username';
-$l['spam_email']      = 'Email Address';
-$l['spam_ip']         = 'IP Address';
-$l['spam_date']       = 'Date';
-$l['spam_confidence'] = 'Confidence';
-$l['no_spam_logs'] = 'No users have been blocked by the spam filters yet.';
-$l['success_pruned_spam_logs'] = 'The spam logs have been pruned successfully.';
-$l['note_logs_locked'] = 'For security reasons, logs less than 24 hours old cannot be pruned.';
-$l['all_usernames'] = 'All usernames';
-$l['all_emails'] = ' All Email Addresses';
-$l['date_range'] = "Date range:";
-$l['days'] = "days";
-$l['filter_spam_logs'] = 'Filter Spam Logs';
-$l['asc'] = "Ascending";
-$l['desc'] = "Descending";
-
-$l['in'] = "in";
-$l['order'] = "order";
-$l['sort_by'] = "Sort By";
-$l['results_per_page'] = "Results Per Page";
+$l['in'] = "em";
+$l['order'] = "ordem";
+$l['sort_by'] = "Ordenar por";
+$l['results_per_page'] = "Resultados por página";
