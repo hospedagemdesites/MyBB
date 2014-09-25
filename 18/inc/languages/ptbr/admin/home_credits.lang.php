d4d98a5437743b431c4309a112813c770390b522
dthiago <thiagoalves@live.com>
2014-09-25 02:07:12 -0300

    Update home_credits.lang.php

--- 18/inc/languages/ptbr/admin/home_credits.lang.php
+++ 18/inc/languages/ptbr/admin/home_credits.lang.php
@@ -1,14 +1,13 @@
 <?php
 /**
- * MyBB 1.8 Pacote de Idioma Português do Brasil
- * Copyright 2014 MyBB Group, All Rights Reserved
- *
+ * MyBB 1.8 Pacote de Língua Brasileira
+ * Direitos Autorais 2014 MyBB Group, Todos os Direitos Reservados
+ * Traduzido por dthiago ArnoldLayne-xXx http://bf4brasil.com.br/
  */
 
-$l['mybb_credits'] = "MyBB Credits";
-$l['mybb_credits_description'] = "These people have contributed their time and effort to create MyBB.";
-$l['about_the_team'] = "About the Team";
-$l['check_for_updates'] = "Check for Updates";
-$l['error_communication'] = "There was a problem downloading the latest credits. Please try again in a few minutes.";
-$l['no_credits'] = "No stored MyBB credits. <a href=\"index.php?module=home-credits&amp;fetch_new=1\">Check for Updates</a>.";
-
+$l['mybb_credits'] = "Créditos do MyBB";
+$l['mybb_credits_description'] = "Essas pessoas contribuíram com o seu tempo e esforço para criar o MyBB.";
+$l['about_the_team'] = "Sobre a Equipe";
+$l['check_for_updates'] = "Verificar atualizações";
+$l['error_communication'] = "Ocorreu um problema ao efetuar o Download  dos últimos créditos. Por favor, tente novamente daqui a alguns minutos.";
+$l['no_credits'] = "Nenhum crédito MyBB armazenado. <a href=\"index.php?module=home-credits&amp;fetch_new=1\"> Procurar por atualizações</a>.";
