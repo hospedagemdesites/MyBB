323306528c0ca82153fec8bacdaefebe7778f370
dthiago <thiagoalves@live.com>
2014-09-25 02:09:06 -0300

    Update tools_optimizedb.lang.php

--- 18/inc/languages/ptbr/admin/tools_optimizedb.lang.php
+++ 18/inc/languages/ptbr/admin/tools_optimizedb.lang.php
@@ -1,20 +1,20 @@
 <?php
 /**
- * MyBB 1.8 Pacote de Idioma Português do Brasil
- * Copyright 2014 MyBB Group, All Rights Reserved
- *
+ * MyBB 1.8 Pacote de Língua Brasileira
+ * Direitos Autorais 2014 MyBB Group, Todos os Direitos Reservados
+ * Traduzido por dthiago ArnoldLayne-xXx http://bf4brasil.com.br/
  */
 
-$l['optimize_database'] = "Optimize Database";
+$l['optimize_database'] = "Otimizar Base de Dados";
 
-$l['table_selection'] = "Table Selection";
-$l['tables_select_desc'] = "You may select the database tables you wish to perform this action on here. Hold down CTRL to select multiple tables.";
-$l['select_all'] = "Select All";
-$l['deselect_all'] = "Deselect All";
-$l['select_forum_tables'] = "Select Forum Tables";
-$l['optimize_selected_tables'] = "Optimize Selected Tables";
+$l['table_selection'] = "Seleção da Tabela";
+$l['tables_select_desc'] = "Você pode selecionar a tabela de base de dados que desejar. Para efetuar esta operação mantenha pressionada a tecla CTRL e selecione múltiplas tabelas.";
+$l['select_all'] = "Selecionar Tudo";
+$l['deselect_all'] = "Remover Seleção";
+$l['select_forum_tables'] = "Selecionar Tabelas do Fórum";
+$l['optimize_selected_tables'] = "Otimizar Tabelas Selecionadas";
 
-$l['error_no_tables_selected'] = "You did not select any database tables to optimize.";
+$l['error_no_tables_selected'] = "Você não selecionou nenhuma tabela da base de dados para otimizar.";
 
-$l['success_tables_optimized'] = "The selected tables have been optimized and analyzed successfully.";
+$l['success_tables_optimized'] = "As tabelas selecionadas foram otimizadas e analisadas com sucesso.";
 
