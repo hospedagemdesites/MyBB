3ba2314656e98a876b1c5f5823a3b019f4331afd
NewtonPDL <modnewton@gmail.com>
2014-09-24 23:12:14 -0300

    Update datahandler_login.lang.php

--- 18/inc/languages/ptbr/datahandler_login.lang.php
+++ 18/inc/languages/ptbr/datahandler_login.lang.php
@@ -3,11 +3,12 @@
  * MyBB 1.8 Pacote de Idioma Português do Brasil
  * Copyright 2014 MyBB Group, All Rights Reserved
  * Traduzido por "Leandro dos Santos" "www.hospedagemecriacaodesites.com.br"
+ * Revisado por "NewtonPDL" "http://community.mybb.com/user-35908.html"
  */
 
-$l['logindata_invalidpwordusername'] = "Você informou uma combinação de nome de usuário e senha inválida. <br /><br />Se você esqueceu sua senha, por favor <a href=\"member.php?action=lostpw\">Recupere uma nova</a>.";
-$l['logindata_invalidpwordusernameemail'] = "Você informou uma combinação de e-mail e senha inválida. <br /><br />Se você esqueceu sua senha, por favor <a href=\"member.php?action=lostpw\">Recupere uma nova</a>.";
-$l['logindata_invalidpwordusernamecombo'] = "Você informou uma combinação de nome de usuário e senha ou e-mail e senha inválida. <br /><br />Se você esqueceu sua senha, por favor <a href=\"member.php?action=lostpw\">Recupere uma nova</a>.";
+$l['logindata_invalidpwordusername'] = "Você informou uma combinação de nome de usuário e senha inválida. <br /><br />Se você esqueceu sua senha, por favor <a href=\"member.php?action=lostpw\">Recupere-a</a>.";
+$l['logindata_invalidpwordusernameemail'] = "Você informou uma combinação de e-mail e senha inválida. <br /><br />Se você esqueceu sua senha, por favor <a href=\"member.php?action=lostpw\">Recupere-a</a>.";
+$l['logindata_invalidpwordusernamecombo'] = "Você informou uma combinação de nome de usuário e senha ou e-mail e senha inválida. <br /><br />Se você esqueceu sua senha, por favor <a href=\"member.php?action=lostpw\">Recupere-a</a>.";
 
 $l['logindata_regimageinvalid'] = "O código de verificação de imagem que você inseriu estava incorreto. Por favor digite o código exatamente como ele aparece na imagem.";
-$l['logindata_regimagerequired'] = "Por favor, preencha o código de verificação de imagem para continuar o processo de logon. Por favor digite o código exatamente como ele aparece na imagem.";
+$l['logindata_regimagerequired'] = "Por favor, preencha o código de verificação da imagem para continuar a entrar. Por favor digite o código exatamente como ele aparece na imagem.";
