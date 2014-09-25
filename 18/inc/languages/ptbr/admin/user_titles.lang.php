e06c4fd530f990deb958e450ef8f42dc9913c8e1
dthiago <thiagoalves@live.com>
2014-09-25 02:08:00 -0300

    Update user_titles.lang.php

--- 18/inc/languages/ptbr/admin/user_titles.lang.php
+++ 18/inc/languages/ptbr/admin/user_titles.lang.php
@@ -1,37 +1,36 @@
 <?php
 /**
- * MyBB 1.8 Pacote de Idioma Português do Brasil
- * Copyright 2014 MyBB Group, All Rights Reserved
- *
+ * MyBB 1.8 Pacote de Língua Brasileira
+ * Direitos Autorais 2014 MyBB Group, Todos os Direitos Reservados
+ * Traduzido por dthiago ArnoldLayne-xXx http://bf4brasil.com.br/
  */
 
-$l['user_titles'] = "User Titles";
-$l['user_titles_desc'] = "This section allows management of user titles. User titles are assigned to users based on the number of posts they make and also allow a custom 'Star' image to be shown based on the number of posts the user has.";
-$l['add_new_user_title'] = "Add New User Title";
-$l['add_new_user_title_desc'] = "This section allows you to add a new user title. <i>Note: This is <strong>not</strong> the <u><a href=\"index.php?module=user-group_promotions\">promotion system.</a></u></i>";
+$l['user_titles'] = "Títulos de Usuários";
+$l['user_titles_desc'] = "Esta seção permite a manutenção dos títulos de usuários. Os títulos de usuários são assinalados aos usuários com base no número de mensagens criados por eles e também permitem uma estrela personalizada que pode ser mostrada com base no número de mensagens criadas pelos usuários.";
+$l['add_new_user_title'] = "Adicionar Título de Usuários";
+$l['add_new_user_title_desc'] = "Esta seção permite-lhe adicionar um novo título de usuários. <i>Observe: Isto <strong>não</strong> é o<u><a href=\"index.php?module=user/group_promotions\">sistema de promoções.</a></u></i>";
 
-$l['error_missing_title'] = "You did not enter a title for this user title";
-$l['error_missing_posts'] = "You did not enter the minimum number of posts for this user title";
-$l['error_cannot_have_same_posts'] = "This user title cannot have the same minimum number of posts as another title";
-$l['error_invalid_user_title'] = "You have specified an invalid user title";
+$l['error_missing_title'] = "Não inseriu um título para este título de usuários.";
+$l['error_missing_posts'] = "Não inseriu um número mínimo de mensagens para este título de usuários.";
+$l['error_cannot_have_same_posts'] = "Este título de usuários não pode ter o mesmo número mínimo de mensagens como outro título";
+$l['error_invalid_user_title'] = "O título de usuário especificado não existe.";
 
-$l['success_user_title_created'] = "The new user title has been created successfully.";
-$l['success_user_title_updated'] = "The user title has been updated successfully.";
-$l['success_user_title_deleted'] = "The specified user title has been deleted successfully.";
+$l['success_user_title_created'] = "O novo título de usuário foi criado com sucesso.";
+$l['success_user_title_updated'] = "O título de usuário especificado foi editado com sucesso.";
+$l['success_user_title_deleted'] = "O título de usuário especificado foi excluido com sucesso.";
 
-$l['title_to_assign'] = "Title to Assign";
-$l['title_to_assign_desc'] = "This title will be shown for users underneath their name if they do not have a custom title set.";
-$l['minimum_posts'] = "Minimum Posts";
-$l['minimum_posts_desc'] = "The minimum number of posts for a user to have before they're assigned this user title.";
-$l['number_of_stars'] = "Number of Stars";
-$l['number_of_stars_desc'] = "Enter the number of stars to be shown under this user title. Set to 0 to show no stars.";
-$l['star_image'] = "Star Image";
-$l['star_image_desc'] = "If this user title should show stars, enter the path to the star image here. If empty, the user group star image will be shown. Use {theme} to specify the image directory for the viewers current theme.";
-$l['save_user_title'] = "Save User Title";
-$l['edit_user_title'] = "Edit User Title";
-$l['edit_user_title_desc'] = "This section allows you to edit a user title.";
-$l['user_title_deletion_confirmation'] = "Are you sure you want to delete this user title?";
-$l['manage_user_titles'] = "Manage User Titles";
-$l['user_title'] = "User Title";
-$l['no_user_titles'] = "You do not have any user titles defined at the moment";
-
+$l['title_to_assign'] = "Título a Atribuir";
+$l['title_to_assign_desc'] = "Este título será mostrado aos usuários abaixo do nome deles caso eles não tenham um título personalizado.";
+$l['minimum_posts'] = "Mensagens Mínimas";
+$l['minimum_posts_desc'] = "O número mínimo de mensagens ao qual é necessário para que este título de usuários seja atribuído a um membro.";
+$l['number_of_stars'] = "Número de Estrelas";
+$l['number_of_stars_desc'] = "Insira o número de estrelas a ser mostrada. Coloque 0 para não mostrar quaisquer estrelas.";
+$l['star_image'] = "Imagem de Estrela";
+$l['star_image_desc'] = "Se este título de usuário deverá mostrar estrelas - insira o caminho para a imagem da estrela  aqui. Caso fique vazio a imagem das estrelas referente ao grupo de Usuários será usada. Use {theme} para especificar a pasta de imagens para o tema atual de cada usuário.";
+$l['save_user_title'] = "Salvar Título de Usuários";
+$l['edit_user_title'] = "Editar Título de Usuários";
+$l['edit_user_title_desc'] = "Esta seção permite-lhe editar um título de usuário.";
+$l['user_title_deletion_confirmation'] = "Tem a certeza deseja excluir este título de usuário?";
+$l['manage_user_titles'] = "Gerenciar Títulos de Usuários";
+$l['user_title'] = "Título de Usuários";
+$l['no_user_titles'] = "Não tem quaisquer títulos de usuários definidos atualmente.";
