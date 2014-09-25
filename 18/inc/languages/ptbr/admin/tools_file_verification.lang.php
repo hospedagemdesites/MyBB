c6d782f8e398b7404a793ce11227224666fa74c4
dthiago <thiagoalves@live.com>
2014-09-25 02:11:07 -0300

    Update tools_file_verification.lang.php

--- 18/inc/languages/ptbr/admin/tools_file_verification.lang.php
+++ 18/inc/languages/ptbr/admin/tools_file_verification.lang.php
@@ -1,20 +1,20 @@
 <?php
 /**
- * MyBB 1.8 Pacote de Idioma Português do Brasil
- * Copyright 2014 MyBB Group, All Rights Reserved
- *
+ * MyBB 1.8 Pacote de Língua Brasileira
+ * Direitos Autorais 2014 MyBB Group, Todos os Direitos Reservados
+ * Traduzido por dthiago ArnoldLayne-xXx http://bf4brasil.com.br/
  */
 
-$l['file_verification'] = "File Verification";
-$l['checking'] = "Checking...";
-$l['file_verification_message'] = "This process will check all MyBB default files to check for validity. Press Yes to continue.<br /> <small>Please note, this process can not differentiate between custom modifications and actual corruption in files. Therefore you should take caution in reverting files this process returns as \"corrupt\".</small>";
+$l['file_verification'] = "Verificação do Arquivo";
+$l['checking'] = "Verificando..";
+$l['file_verification_message'] = "Este processo irá verificar a validação de todos os arquivos padrão. Clique em Sim para continuar.<br /> <small>Atenção, este processo não diferencia  modificações personalizadas e arquivos atualmente corrompidos. Por esse motivo tenha atenção ao reverter os arquivos pois este processo pode retornar como \"corrompido\".</small>";
 
-$l['error_communication'] = "There was a problem communicating with the MyBB server. Please try again in a few minutes.";
-$l['file'] = "File";
-$l['no_corrupt_files_found'] = "Congratulations! No corrupt files have been found on your installation.";
-$l['found_problems'] = "Found Problems";
-$l['no_problems_found'] = "No Problems Detected";
-$l['changed'] = "Changed";
-$l['missing'] = "Missing";
-$l['status'] = "Status";
+$l['error_communication'] = "Existe um problema de comunicação com o servidor MyBB. Por favor tente novamente, em alguns minutos.";
+$l['file'] = "Arquivo";
+$l['no_corrupt_files_found'] = "Parabéns! Não foram detetados arquivos corrompidos na sua instalação.";
+$l['found_problems'] = "Problemas Encontrados";
+$l['no_problems_found'] = "Não foram detetados problemas";
+$l['changed'] = "Modificados";
+$l['missing'] = "Perdidos";  // Poderia ser "Em Falta" ou "Corrompidos"
+$l['status'] = "Estado";
 
