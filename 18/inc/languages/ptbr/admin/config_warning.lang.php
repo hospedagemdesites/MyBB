b75a8a83c4e41008f6aa50dc5cdd647b186b9944
dthiago <thiagoalves@live.com>
2014-09-26 02:09:49 -0300

    Update config_warning.lang.php

--- 18/inc/languages/ptbr/admin/config_warning.lang.php
+++ 18/inc/languages/ptbr/admin/config_warning.lang.php
@@ -1,79 +1,79 @@
 <?php
 /**
- * MyBB 1.8 Pacote de Idioma Português do Brasil
- * Copyright 2014 MyBB Group, All Rights Reserved
- *
+ * MyBB 1.8 Pacote de Língua Brasileira
+ * Direitos Autorais 2014 MyBB Group, Todos os Direitos Reservados
+ * Traduzido por dthiago ArnoldLayne-xXx http://bf4brasil.com.br/
  */
 
-$l['warning_system'] = "Warning System";
-$l['warning_types'] = "Warning Types";
-$l['warning_types_desc'] = "Here you can manage the list of different warning types staff are allowed to issue to users.";
-$l['add_warning_type'] = "Add New Warning Type";
-$l['add_warning_type_desc'] = "Here you can create a new predefined warning type. Warning types are selectable when warning users and you can define the number of points to add for this type as well as the time period before warnings of this type expire.";
-$l['edit_warning_type'] = "Edit Warning Type";
-$l['edit_warning_type_desc'] = "Here you can edit this warning type. Warning types are selectable when warning users and you can define the number of points to add for this type as well as the time period before warnings of this type expire.";
-$l['warning_levels'] = "Warning Levels";
-$l['warning_levels_desc'] = "Warning Levels define what happens to a user when they reach a particular warning level (percentage of maximum warning points). You can ban users or suspend their privileges.";
-$l['add_warning_level'] = "Add New Warning Level";
-$l['add_warning_level_desc'] = "Here you can create a new warning level. Warning levels are actions to be taken against users when they reach a specific percentage of the maximum warning level.";
-$l['edit_warning_level'] = "Edit Warning Level";
-$l['edit_warning_level_desc'] = "Warning levels are actions to be taken against users when they reach a specific percentage of the maximum warning level.";
+$l['warning_system'] = "Sistema de Avisos";
+$l['warning_types'] = "Tipos de Aviso";
+$l['warning_types_desc'] = "Aqui pode gerenciar a lista de diferentes tipos de aviso que a equipe do fórum está autorizada a usar.";
+$l['add_warning_type'] = "Adicionar Novo Aviso";
+$l['add_warning_type_desc'] = "Aqui você pode criar um novo tipo de aviso predefinido. Tipos de avisos são selecionáveis quando um usuário é avisado e você possa definir o número de pontos a adicionar e também o período de tempo necessário para o aviso expirar.";
+$l['edit_warning_type'] = "Editar Aviso";
+$l['edit_warning_type_desc'] = "Aqui você pode editar o tipo de aviso. Tipos de avisos são selecionáveis quando um usuário é avisado e você possa definir o número de pontos a adicionar e também o período de tempo necessário para o aviso expirar.";
+$l['warning_levels'] = "Nível de Aviso";
+$l['warning_levels_desc'] = "Níveis de Aviso definem o que acontece a um usuário quando eles chegam a um determinado  "Nível de Aviso" (percentagem máxima de pontos de aviso ). Você pode expulsar usuários ou suspender os seus privilégios.";
+$l['add_warning_level'] = "Adicionar Novo Nível de Aviso";
+$l['add_warning_level_desc'] = "Aqui você pode criar um novo nível de aviso. Níveis de aviso são ações tomadas contra usuários quando atingem uma percentagem específica do máximo nível de aviso.";
+$l['edit_warning_level'] = "Editar Nível de Aviso";
+$l['edit_warning_level_desc'] = "Níveis de aviso são acões tomadas contra usuários quando atingem uma percentagem específica do máximo nível de aviso.";
 
-$l['percentage'] = "Percentage";
-$l['action_to_take'] = "Action to Take";
-$l['move_banned_group'] = "Move to banned group ({3}) for {1} {2}";
-$l['move_banned_group_permanent'] = "Move to banned group ({1}) Permanently";
-$l['suspend_posting'] = "Suspend posting privileges for {1} {2}";
-$l['suspend_posting_permanent'] = "Suspend posting privileges Permanently";
-$l['moderate_new_posts'] = "Moderate new posts for {1} {2}";
-$l['moderate_new_posts_permanent'] = "Moderate new posts Permanently";
-$l['no_warning_levels'] = "There are no warning levels on your forum at this time.";
+$l['percentage'] = "Percentagem";
+$l['action_to_take'] = "Ação a Executar";
+$l['move_banned_group'] = "Mover para o grupo de banidos ({3}) por {1} {2}";
+$l['move_banned_group_permanent'] = "Mover para o grupo de banidos ({1}) permanentemente";
+$l['suspend_posting'] = "Suspender privilégios postagem por {1} {2}";
+$l['suspend_posting_permanent'] = "Suspender privilégios de postagem permanentemente";
+$l['moderate_new_posts'] = "Moderar novas mensagens para {1} {2}";
+$l['moderate_new_posts_permanent'] = "Moderar novas mensagens permanentemente";
+$l['no_warning_levels'] = "Não existem Níveis de aviso no seu fórum neste momento.";
 
-$l['warning_type'] = "Warning Type";
-$l['points'] = "Points";
-$l['expires_after'] = "Expires After";
-$l['no_warning_types'] = "There are no warning types on your forum at this time.";
+$l['warning_type'] = "Tipo de Aviso";
+$l['points'] = "Pontos";
+$l['expires_after'] = "Expiram Após";
+$l['no_warning_types'] = "Não existem tipos de aviso no seu fórum neste momento.";
 
-$l['warning_points_percentage'] = "Percentage of Maximum Warning Points";
-$l['warning_points_percentage_desc'] = "Please enter a numeric value between 1 and 100.";
-$l['action_to_be_taken'] = "Action to be Taken";
-$l['action_to_be_taken_desc'] = "Select the action you wish to be taken when users reach the above level.";
-$l['ban_user'] = "Ban User";
-$l['banned_group'] = "Banned group:";
-$l['ban_length'] = "Ban length:";
-$l['suspend_posting_privileges'] = "Suspend Posting Privileges";
-$l['suspension_length'] = "Suspension length:";
-$l['moderate_posts'] = "Moderate Posts";
-$l['moderation_length'] = "Moderation length:";
-$l['save_warning_level'] = "Save Warning Level";
+$l['warning_points_percentage'] = "Percentagem Máxima de Pontos de Aviso";
+$l['warning_points_percentage_desc'] = "Por favor insira um valor numérico entre 1 e 100.";
+$l['action_to_be_taken'] = "Ações a Tomar";
+$l['action_to_be_taken_desc'] = "Selecione a ação que deseja tomar quando os usuários atingem um nível elevado.";
+$l['ban_user'] = "Expulsar usuário";
+$l['banned_group'] = "Grupo Expulso:";
+$l['ban_length'] = "Tempo da Expulsão:";
+$l['suspend_posting_privileges'] = "Suspender privilégios de postagem";
+$l['suspension_length'] = "Tempo da suspensão:";
+$l['moderate_posts'] = "Moderar Mensagens";
+$l['moderation_length'] = "Tempo de Moderação:";
+$l['save_warning_level'] = "Salvar Nível de Aviso";
 
-$l['title'] = "Title";
-$l['points_to_add'] = "Points to Add";
-$l['points_to_add_desc'] = "The number of points to add to a users warning level.";
-$l['warning_expiry'] = "Warning Expiry";
-$l['warning_expiry_desc'] = "How long after this warning is given do you want it to expire?";
-$l['save_warning_type'] = "Save Warning Type";
+$l['title'] = "Título";
+$l['points_to_add'] = "Pontos para adição";
+$l['points_to_add_desc'] = "O número de pontos a adicionar ao nível de aviso dos usuários.";
+$l['warning_expiry'] = "Validade do Nível";
+$l['warning_expiry_desc'] = "Qual o tempo de aviso após expiração?";
+$l['save_warning_type'] = "Salvar Tipo de Aviso";
 
-$l['expiration_hours'] = "Hour(s)";
-$l['expiration_days'] = "Day(s)";
-$l['expiration_weeks'] = "Week(s)";
-$l['expiration_months'] = "Month(s)";
-$l['expiration_never'] = "Never";
-$l['expiration_permanent'] = "Permanent";
+$l['expiration_hours'] = "Hora(s)";
+$l['expiration_days'] = "Dia(s)";
+$l['expiration_weeks'] = "Semana(s)";
+$l['expiration_months'] = "Mês(es)";
+$l['expiration_never'] = "Nunca";
+$l['expiration_permanent'] = "Permanente";
 
-$l['error_invalid_warning_level'] = "The specified warning level does not exist.";
-$l['error_invalid_warning_percentage'] = "You did not enter a valid percentage value for this warning level. Your percentage value must be between 1 and 100.";
-$l['error_invalid_warning_type'] = "The specified warning type does not exist.";
-$l['error_missing_type_title'] = "You did not enter a title for this warning type";
-$l['error_missing_type_points'] = "You did not enter a valid number of points to add when giving warnings of this type. You must enter a number greater than 0 but not greater than {1}";
+$l['error_invalid_warning_level'] = "O Nível de Aviso especificado não existe.";
+$l['error_invalid_warning_percentage'] = "Você não inseriu uma percentagem válida para este Nível de aviso. O valor da sua percentagem tem de ser entre 1 e 100.";
+$l['error_invalid_warning_type'] = "O Tipo de aviso especificado não existe.";
+$l['error_missing_type_title'] = "Você não inseriu um título para este tipo de aviso";
+$l['error_missing_type_points'] = "Você não inseriu um número de pontos válido para adicionar quando é dado avisos deste tipo. Você tem de inserir um número maior que 0 e menor que {1}";
 
-$l['success_warning_level_created'] = "The warning level has been created successfully.";
-$l['success_warning_level_updated'] = "The warning level has been updated successfully.";
-$l['success_warning_level_deleted'] = "The selected warning level has been deleted successfully.";
-$l['success_warning_type_created'] = "The warning type has been created successfully.";
-$l['success_warning_type_updated'] = "The warning type has been updated successfully.";
-$l['success_warning_type_deleted'] = "The selected warning type has been deleted successfully.";
+$l['success_warning_level_created'] = "O Nível de aviso foi criado com sucesso.";
+$l['success_warning_level_updated'] = "O Nível de aviso foi atualizado com sucesso.";
+$l['success_warning_level_deleted'] = "O Nível de aviso selecionado foi excluído com suesso.";
+$l['success_warning_type_created'] = "O tipo de aviso foi criado com sucesso.";
+$l['success_warning_type_updated'] = "O tipo de aviso foi atualizado com sucesso.";
+$l['success_warning_type_deleted'] = "O tipo de aviso selecionado foi excluído com sucesso.";
 
-$l['confirm_warning_level_deletion'] = "Are you sure you wish to delete this warning level?";
-$l['confirm_warning_type_deletion'] = "Are you sure you wish to delete this warning type?";
+$l['confirm_warning_level_deletion'] = "Tem a certeza deseja excluir este nível de aviso?";
+$l['confirm_warning_type_deletion'] = "Tem a certeza deseja excluir este tipo de aviso?";
 
