6c7d0c5d3d4898eea833292bc72eb276b3a29467
dthiago <thiagoalves@live.com>
2014-09-26 02:11:23 -0300

    Update tools_recount_rebuild.lang.php

--- 18/inc/languages/ptbr/admin/tools_recount_rebuild.lang.php
+++ 18/inc/languages/ptbr/admin/tools_recount_rebuild.lang.php
@@ -1,52 +1,52 @@
 <?php
 /**
- * MyBB 1.8 Pacote de Idioma Português do Brasil
- * Copyright 2014 MyBB Group, All Rights Reserved
- *
+ * MyBB 1.8 Pacote de Língua Brasileira
+ * Direitos Autorais 2014 MyBB Group, Todos os Direitos Reservados
+ * Traduzido por dthiago ArnoldLayne-xXx http://bf4brasil.com.br/
  */
 
-$l['recount_rebuild'] = "Recount &amp; Rebuild";
-$l['recount_rebuild_desc'] = "Here you can recount &amp; rebuild data to fix any synchronization errors in your forum.";
+$l['recount_rebuild'] = "Recontar &amp; Reconstruir";
+$l['recount_rebuild_desc'] = "Aqui você pode recontar e reconstruir as informação dos dados para corrigir erros de sincronização no seu fórum.";
 
-$l['data_per_page'] = "Data Entries Per Page";
-$l['recount_stats'] = "Recount Statistics";
-$l['recount_stats_desc'] = "This will recount and update your forum statistics on the forum index and statistics pages.";
-$l['recount_reputation'] = "Recount Reputation";
-$l['recount_reputation_desc'] = "This will recount the reputation of each user.";
-$l['recount_warning'] = "Recount Warning Points";
-$l['recount_warning_desc'] = "This will recount the active warning points of each user.";
-$l['recount_private_messages'] = "Recount Private Messages";
-$l['recount_private_messages_desc'] = "This will recount the private message count for each user.";
-$l['recount_referrals'] = "Recount Referrals";
-$l['recount_referrals_desc'] = "This will recount the referral count of each user.";
-$l['recount_thread_ratings'] = "Recount Thread Ratings";
-$l['recount_thread_ratings_desc'] = "This will recount the rating of each thread.";
-$l['rebuild_forum_counters'] = "Rebuild Forum Counters";
-$l['rebuild_forum_counters_desc'] = "When this is run, the post/thread counters and last post of each forum will be updated to reflect the correct values.";
-$l['rebuild_thread_counters'] = "Rebuild Thread Counters";
-$l['rebuild_thread_counters_desc'] = "When this is run, the post/view counters and last post of each thread will be updated to reflect the correct values.";
-$l['rebuild_poll_counters'] = "Rebuild Poll Counters";
-$l['rebuild_poll_counters_desc'] = "When this is run, the vote counters and total number of votes of each poll will be updated to reflect the correct values.";
-$l['recount_user_posts'] = "Recount User Post Counts";
-$l['recount_user_posts_desc'] = "When this is run, the post count for each user will be updated to reflect its current live value based on the posts in the database, and forums that have post count disabled.";
-$l['recount_user_threads'] = "Recount User Thread Counts";
-$l['recount_user_threads_desc'] = "When this is run, the thread count for each user will be updated to reflect its current live value based on the threads in the database, and forums that have thread count disabled.";
-$l['rebuild_attachment_thumbs'] = "Rebuild Attachment Thumbnails";
-$l['rebuild_attachment_thumbs_desc'] = "This will rebuild attachment thumbnails to ensure they're using the current width and height dimensions and will also rebuild missing thumbnails.";
+$l['data_per_page'] = "Entradas por Página";
+$l['recount_stats'] = "Recontagem das Estatísticas ";
+$l['recount_stats_desc'] = "Isto irá recontar e atualizar as estatísticas do seu fórum, no início do fórum e página de estatísticas.";
+$l['recount_reputation'] = "Recontagem e Reputação";
+$l['recount_reputation_desc'] = "Isto irá recontar a reputação de cada usuário.";
+$l['recount_warning'] = "Recontar Pontos de Aviso";
+$l['recount_warning_desc'] = "Isto irá recontar os pontos de aviso ativos de cada usuário.";
+$l['recount_private_messages'] = "Recontar Mensagens Privadas";
+$l['recount_private_messages_desc'] = "Isto irá recontar a contagem e mensagens privadas de cada usuário.";
+$l['recount_referrals'] = "Recontar Referências";
+$l['recount_referrals_desc'] = " Isto irá recontar a contagem de referências de cada usuário.";
+$l['recount_thread_ratings'] = "Recontar Avaliações de Tópicos";
+$l['recount_thread_ratings_desc'] = "Isto irá recontar a avaliação de cada tópico.";
+$l['rebuild_forum_counters'] = "Reconstruir Contagem do Fórum";
+$l['rebuild_forum_counters_desc'] = "Quando executado as mensagens , contagem de tópicos e últimas mensagens de cada fórum são atualizados para refletir os valores corretos.";
+$l['rebuild_thread_counters'] = "Reconstruir Contagem de Tópicos";
+$l['rebuild_thread_counters_desc'] = "Quando executado, as mensagens/ visualizadores de contadores e última mensagem de cada tópico são atualizados para refletir os valores corretos.";
+$l['rebuild_poll_counters'] = "Reconstruir Contagem de Votações";
+$l['rebuild_poll_counters_desc'] = "Quando esta operação é executada - a contagem de votos e o número total de votos é actualizada para refletir nos valores corretos.";
+$l['recount_user_posts'] = "Recontar Contagem de Mensagens de usuários";
+$l['recount_user_posts_desc'] = "Quando executado a contagem de mensagens para cada usuário irá ser atualizada para refletir o valor atual no momento com base nas mensagens da base de dados e que tenham a contagem de mensagens desativada.";
+$l['recount_user_threads'] = "Recontar a contagem de tópicos de usuário";
+$l['recount_user_threads_desc'] = "Quando esta operação é executada a contagem de tópicos de cada usuário é atualizada em tempo real para refletir seu valor  com base nos  tópicos e nos fóruns onde a contagem de tópicos esteja desativada.";
+$l['rebuild_attachment_thumbs'] = "Reconstruir Miniaturas dos Anexos";
+$l['rebuild_attachment_thumbs_desc'] = "Isto irá reconstruir as miniaturas de anexos para garantir que usam as dimensões de largura e altura corrente e também reconstruir miniaturas em falta.";
 
-$l['success_rebuilt_forum_counters'] = "The forum counters have been rebuilt successfully.";
-$l['success_rebuilt_thread_counters'] = "The thread counters have been rebuilt successfully.";
-$l['success_rebuilt_poll_counters'] = "The poll counters have been rebuilt successfully.";
-$l['success_rebuilt_user_post_counters'] = "The user posts count have been recounted successfully.";
-$l['success_rebuilt_user_thread_counters'] = "The user threads count have been recounted successfully.";
-$l['success_rebuilt_attachment_thumbnails'] = "The attachment thumbnails have been rebuilt successfully.";
-$l['success_rebuilt_forum_stats'] = "The forum statistics have been rebuilt successfully.";
-$l['success_rebuilt_reputation'] = "The user reputation has been rebuilt successfully.";
-$l['success_rebuilt_warning'] = "The user warning points have been rebuilt successfully.";
-$l['success_rebuilt_private_messages'] = "The user private message count has been recounted successfully.";
-$l['success_rebuilt_referral'] = "The user referral count have been recounted successfully.";
-$l['success_rebuilt_thread_ratings'] = "The thread ratings have been recounted successfully.";
+$l['success_rebuilt_forum_counters'] = "A contagem de fóruns foi reconstruída com sucesso.";
+$l['success_rebuilt_thread_counters'] = "A contagem de tópicos foi reconstruída com sucesso.";
+$l['success_rebuilt_poll_counters'] = "A contagem de votações foi reconstruída com sucesso.";
+$l['success_rebuilt_user_post_counters'] = "A contagem de mensagens de usuário foi recontada com sucesso.";
+$l['success_rebuilt_user_thread_counters'] = "A contagem de tópicos de usuário foi recontada com sucesso.";
+$l['success_rebuilt_attachment_thumbnails'] = "As miniaturas de anexos foram reconstruídas com sucesso.";
+$l['success_rebuilt_forum_stats'] = "As estatísticas do fórum foram reconstruídas com sucesso.";
+$l['success_rebuilt_reputation'] = "A contagem de reputação de usuário foi reconstruída com sucesso.";
+$l['success_rebuilt_warning'] = " A contagem de pontos de aviso de usuário foi reconstruída com sucesso.";
+$l['success_rebuilt_private_messages'] = " A contagem de mensagens privadas de usuário foi recontada com sucesso .";
+$l['success_rebuilt_referral'] = "A contagem de referência de usuário foi recontada com sucesso.";
+$l['success_rebuilt_thread_ratings'] = "A contagem de avaliação de tópicos de usuário foi recontada com sucesso.";
 
-$l['confirm_proceed_rebuild'] = "Click \"Proceed\" to continue the recount and rebuild process.";
-$l['automatically_redirecting'] = "Automatically Redirecting...";
+$l['confirm_proceed_rebuild'] = "Clique \"Continuar\" para continuar o processo de recontagem e reconstrução.";
+$l['automatically_redirecting'] = "Redirecionamento Automático...";
 
