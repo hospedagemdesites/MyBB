baee3c86faf17817ca913489a5abaa979d33690d
ArnoldLayne-xXx <thiagoalves@live.com>
2014-10-31 01:02:26 -0200

    Update ptbr.php

--- 18/inc/languages/ptbr.php
+++ 18/inc/languages/ptbr.php
@@ -6,27 +6,11 @@
  * Traduzido por dthiago ArnoldLayne-xXx "http://bf4brasil.com.br/"
  * Revisado por NewtonPDL
  */
-
-// The friendly name of the language
 $langinfo['name'] = "Língua Brasileira";
-
-// The author of the language
-$langinfo['author'] = "Leandro dos Santos";"ArnoldLayne-xXx";
-
-// The language authors website
-$langinfo['website'] = "http://www.hospedagemecriacaodesites.com.br/";"http://bf4brasil.com.br/"
-
-// Compatible version of MyBB
-$langinfo['version'] = "1800";
-
-// Sets if the translation includes the Admin CP (1 = yes, 0 = no)
+$langinfo['author'] = "Leandro dos Santos, dthiago ArnoldLayne-xXx";
+$langinfo['website'] = "http://www.hospedagemecriacaodesites.com.br/, http://bf4brasil.com.br/";
+$langinfo['version'] = "1801";
 $langinfo['admin'] = 1;
-
-// Sets if the language is RTL (Right to Left) (1 = yes, 0 = no)
 $langinfo['rtl'] = 0;
-
-// Sets the lang in the <html> on all pages
 $langinfo['htmllang'] = "pt-BR";
-
-// Sets the character set, blank uses the default.
 $langinfo['charset'] = "UTF-8";
